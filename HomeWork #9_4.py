b89637cf0909d049c81aa202552bdd7cb3836e99
Andrew <anryzhakov@yandex.ru>
2024-11-10 20:33:02 +0300

    Add files via upload

---
+++ HomeWork #9_4.py
@@ -0,0 +1,43 @@
+# Слушатель курса Junior Python-разработчик
+# Рыжаков Андрей Николаевич, anryzhakov@yandex.ru
+# Домашнее задание по теме "Создание функций на лету"
+
+from random import choice
+
+
+# Lambda-функция:
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map(lambda x, y: x == y, first, second)))
+
+
+# Замыкание:
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for data in data_set:
+                file.write(f'{data}\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+# Метод __call__:
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
